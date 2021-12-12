eb554027c30f6233926ce44f2afee6668ab9a25e
Olivier Hoareau <oha@greenberets.io>
2021-12-13 00:09:34 +0100

    fix: fix

--- src/errors/DocumentNotFoundError.ts
+++ src/errors/DocumentNotFoundError.ts
@@ -5,7 +5,5 @@ export default class DocumentNotFoundError extends AbstractError {
     constructor(type, value, key: string = 'id') {
         value = ('string' === typeof value) ? value : JSON.stringify(value);
         super(`Unknown ${type} ${'id' === key ? '' : `with ${key} is `}'${value}'`, 404, 'document-not-found', {}, {type, key: key, value: value, [key]: value});
-        this.type = type;
-        this[key] = value;
     }
 }
