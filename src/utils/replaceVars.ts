6b39410a26a7a231c436ac58f709af83b214c6d6
Olivier Hoareau <oha@greenberets.io>
2022-03-16 11:23:21 +0100

    fix: add feature of url filter

--- src/utils/replaceVars.ts
+++ src/utils/replaceVars.ts
@@ -3,7 +3,19 @@ import replaceFn from "./replaceFn";
 export function replaceVars(pattern, data = {}, startTagPattern = '\\{\\{', endTagPattern = '\\}\\}') {
     pattern = replaceFn(pattern, (x: string) => ((x.slice(0, 12) === 'process.env.') ? process.env[x.slice(12)] : undefined) || '', '\\[\\[', '\\]\\]');
 
-    return replaceFn(pattern, k => ('undefined' === typeof data[k]) ? '' : data[k], startTagPattern, endTagPattern);
+    const replacer = (k: string) => {
+        const [kk, filter = undefined] = k.split('|') as [string, string?];
+        let value: any = ('undefined' === typeof data[kk]) ? undefined : data[kk];
+        switch (filter) {
+            case 'url': value = encodeURIComponent(value); break;
+            case 'upper': value = (value || '').toUpperCase(); break;
+            case 'lower': value = (value || '').toLowerCase(); break;
+            default: break;
+        }
+        return value || '';
+    }
+    
+    return replaceFn(pattern, replacer, startTagPattern, endTagPattern);
 }
 
 export default replaceVars;
