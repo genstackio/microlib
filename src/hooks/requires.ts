0321fdc136ea87d2ac169aa79253148b34fd5044
Olivier Hoareau <oha@greenberets.io>
2022-06-23 18:58:33 +0200

    fix: fix

--- src/hooks/requires.ts
+++ src/hooks/requires.ts
@@ -1,17 +1,17 @@
 // noinspection JSUnusedGlobalSymbols,JSUnusedLocalSymbols
 export default ({model}) => async (query, mode: string = 'item') => {
     const selectedFields = (query.fields && query.fields.length) ? query.fields : Object.keys(model.dynamics || {});
-    query.fields = Object.keys(buildFields(selectedFields, model));
+    query.fields = buildFields(selectedFields, model);
     return query;
 }
 
 function buildFields(fields, model, all = {}) {
-    return fields.reduce((acc, k) => {
+    return Object.keys(fields.reduce((acc, k) => {
         if (all[k]) return acc;
         acc[k] = true;
-        return buildFields((model.requires || {})[k] || [], model, {...all, ...acc}).reduce((acc2, k2) => {
+        return {...acc, ...buildFields((model.requires || {})[k] || [], model, {...all, ...acc}).reduce((acc2, k2) => {
             acc2[k2] = true;
             return acc2;
-        }, acc);
-    }, {} as any);
+        }, acc)};
+    }, {} as any));
 }
