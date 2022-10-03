f8397e91d162699f9a39dd4639d910b91d228885
Olivier Hoareau <oha@greenberets.io>
2022-10-03 13:54:42 +0200

    feat: add transformers for mozjexl

--- src/utils/evaluate.ts
+++ src/utils/evaluate.ts
@@ -1,5 +1,24 @@
+const transformers = {
+    upper: v => ('string' === typeof v) ? v.toUpperCase() : v,
+    lower: v => ('string' === typeof v) ? v.toLowerCase() : v,
+    split: (v, char) => ('string' === typeof v) ? v.split(char) : [],
+    date: v => ('string' === typeof v) ? new Date(Date.parse(v)) : (('number' === typeof v) ? new Date(v) : undefined),
+    keys: v => ('object' === typeof v) ? Object.keys(v) : (Array.isArray(v) ? v.keys() : []),
+    values: v => ('object' === typeof v) ? Object.values(v) : (Array.isArray(v) ? v.values() : []),
+    len: v => v.length,
+};
+
+let m: any = undefined;
+
+export function getMozjexl() {
+    if (!m) {
+        m = require('mozjexl');
+        Object.entries(transformers).forEach(([k, v]) => m.addTransform(k, v));
+    }
+    return m;
+}
 export async function evaluate(expression: string, vars: any = {}) {
-    return require('mozjexl').eval(expression, vars)
+    return getMozjexl().eval(expression, vars)
 }
 
 export default evaluate;
