b88d4282fddc77248e3bbd0fc53a1a629740ec1d
Olivier Hoareau <oha@greenberets.io>
2022-11-01 18:46:44 +0100

    fix: fix

--- src/converters.ts
+++ src/converters.ts
@@ -1,5 +1,4 @@
 import {buildGqlSelectionInfos, replaceVars} from "./utils";
-import {s3} from "@ohoareau/aws";
 
 export const upper = () => v => `${v}`.toUpperCase();
 export const lower = () => v => `${v}`.toLowerCase();
