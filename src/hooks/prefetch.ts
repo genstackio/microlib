827ab701d7518bd188ddec1eb1f6d32c2c71b524
Olivier Hoareau <oha@greenberets.io>
2022-11-05 15:10:24 +0100

    feat: add requires as prefetchs when needed (dynamically)

--- src/hooks/prefetch.ts
+++ src/hooks/prefetch.ts
@@ -1,13 +1,40 @@
 import caller from '../services/caller';
+import d from 'debug';
 
-export default ({model: {name, prefetchs = {}}, operationName, dir}) => async data => {
-    if (!prefetchs || !prefetchs[operationName]) return data;
-    const fields = Object.keys(prefetchs[operationName]);
-    if (!fields.length) return data;
+const debugHookPrefetch = d('micro:hooks:update-stats');
+
+// noinspection JSUnusedGlobalSymbols
+export default config => async query => {
+    let fields = Object.keys({
+        ...(await buildFieldsFromRequires(config, query)),
+        ...(await buildFieldsFromPrefetchs(config, query))
+    });
+
+    debugHookPrefetch('need to prefetch %j', fields);
+
+    if (!fields.length) return query;
+
+    fields = fields.sort();
+    const {model: {name}, dir} = config;
     let need = true;
-    if (data.oldData) need = !!fields.find(f => undefined === data.oldData[f]);
-    if (need) {
-        data.oldData = await caller.execute(`${name}_get`, {id: data.id, fields}, `${dir}/services/crud`);
-    }
-    return data;
+
+    if (query.oldData) need = !!fields.find(f => undefined === query.oldData[f]);
+    if (need) query.oldData = await caller.execute(`${name}_get`, {id: query.id, fields}, `${dir}/services/crud`);
+
+    return query;
+}
+
+async function buildFieldsFromRequires({model: {requires = {}}}, query: any) {
+    let requestedFields = Object.keys([...(query?.fields || []), ...(query?.selections?.items?.fields || [])].reduce((acc, k) => Object.assign(acc, {[k]: true}), {} as any));
+    requestedFields = requestedFields.sort();
+
+    return requestedFields.reduce((acc, k) => {
+        return (requires[k] || []).reduce((acc2, kk) => Object.assign(acc2, {[kk]: true}), acc);
+    }, {} as any);
+}
+
+// noinspection JSUnusedLocalSymbols
+async function buildFieldsFromPrefetchs({model: {prefetchs = {}}, operationName}, query: any) {
+    if (!prefetchs || !prefetchs[operationName]) return {};
+    return prefetchs[operationName];
 }
