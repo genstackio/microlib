3f5bbfd59f3cfe17bbfa9f1c178dd36558356d7d
Olivier Hoareau <oha@greenberets.io>
2023-06-15 16:04:02 +0200

    feat: add detection for unchanged value in autoTransition

--- src/hooks/auto-transitions.ts
+++ src/hooks/auto-transitions.ts
@@ -12,9 +12,11 @@ export default ({model: {name, autoTransitionTo = {}}, dir}) => async data => {
     const fields = Object.keys(autoTransitionTo);
     if (!fields.length) return data;
     const computedData = fields.reduce((acc, k) => {
-        acc[k] = buildTransitionValue(autoTransitionTo[k]);
+        const vv = buildTransitionValue(autoTransitionTo[k]);
+        if ('**unchanged**' !== vv) acc[k] = vv;
         return acc;
     }, {});
+    if (!computedData || !Object.keys(computedData).length) return data;
     const result = await caller.execute(`${name}_update`, {id: data.id, data: computedData}, `${dir}/services/crud`);
     return {...(data || {}), ...(result || {})};
 }
