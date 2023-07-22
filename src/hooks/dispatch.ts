de0ec8df84239378d7f6c336a40c60f1016c3da1
Olivier Hoareau <oha@greenberets.io>
2023-07-22 14:37:57 +0200

    fix: fix eventbridge json serialization

--- src/hooks/dispatch.ts
+++ src/hooks/dispatch.ts
@@ -9,7 +9,7 @@ export default ({o}) => async data => {
             typeName: o.replace(/_[^_]+$/, ''),
             operation: o.replace(/^.+_([^_]+)$/, '$1'),
         },
-        topic: process.env.DISPATCH_TOPIC_ARN,
+        topic: process.env.DISPATCH_TOPIC_ARN || '',
     });
     return data;
 }
