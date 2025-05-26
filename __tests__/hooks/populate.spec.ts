6fb7478a5d4c02a2cd595d3051c901c04d33bd35
Olivier Hoareau <oha@greenberets.io>
2025-05-26 10:18:48 +0200

    feat: multi-pass for populators

---
+++ __tests__/hooks/populate.spec.ts
@@ -0,0 +1,62 @@
+import populate from '../../src/hooks/populate';
+
+describe('populate', () => {
+    [
+        ['empty return empty',
+            {},
+            {},
+            { autoPopulated: {} },
+        ],
+        ['non empty but no values return same',
+            {},
+            { data: { a: 42 } },
+            { data: { a: 42 }, autoPopulated: {} },
+        ],
+        ['non empty with values but on property that have a value return provided value',
+            { values: { b: {type: '@value', config: {value: "World"}} }},
+            { data: { a: 42, b: "Hello" } },
+            { data: { a: 42, b: "World" }, autoPopulated: { b: true} },
+        ],
+        ['non empty with values and property that have no value return value for that property',
+            { values: { b: {type: '@value', config: {value: "World"}} }},
+            { data: { a: 42, c: "Hello" } },
+            { data: { a: 42, b: "World", c: "Hello" }, autoPopulated: { b: true } },
+        ],
+        ['non empty with integer value and property that have no value return value for that property',
+            { values: { b: {type: '@value', config: {value: 43}} }},
+            { data: { a: 42, c: "Hello" } },
+            { data: { a: 42, b: 43, c: "Hello" }, autoPopulated: { b: true } },
+        ],
+        ['non empty with integer value and property that have value return value for that property',
+            { values: { b: {type: '@value', config: {value: 43}} }},
+            { data: { a: 42, b: 41, c: "Hello" } },
+            { data: { a: 42, b: 43, c: "Hello" }, autoPopulated: { b: true } },
+        ],
+        ['non empty with integer value and property that have value return value for that property',
+            { values: { b: {type: '@value', config: {value: 43}} }},
+            { data: { a: 42, b: 0, c: "Hello" } },
+            { data: { a: 42, b: 43, c: "Hello" }, autoPopulated: { b: true } },
+        ],
+        ['non empty with boolean value and property that have value return value for that property',
+            { values: { b: {type: '@value', config: {value: false}} }},
+            { data: { a: 42, b: true, c: "Hello" } },
+            { data: { a: 42, b: false, c: "Hello" }, autoPopulated: { b: true } },
+        ],
+        ['non empty with boolean value and property that have value return value for that property',
+            { values: { b: {type: '@value', config: {value: true}} }},
+            { data: { a: 42, b: false, c: "Hello" } },
+            { data: { a: 42, b: true, c: "Hello" }, autoPopulated: { b: true } },
+        ],
+        ['multi-passes values',
+            { values: { b: {type: '@value', pass: 2, config: {value: '{{z}}'}}, z: {type: '@value', config: {value: 12}} }},
+            { data: { a: 42, b: false, c: "Hello" } },
+            { data: { a: 42, b: "12", c: "Hello", z: 12 }, autoPopulated: { b: true, z: true } },
+        ],
+    ]
+        .forEach(
+            ([name, model, data, expected]: any) => it(name, async () => {
+                expect(await populate({model, dir: undefined})(data)).toEqual(expected);
+            }
+        ))
+    ;
+});
--- src/hooks/populate.ts
+++ src/hooks/populate.ts
@@ -13,16 +13,33 @@ export default ({model, dir, prefix = undefined}) => async data => {
     const valuesKey = prefix ? `${prefix}Values` : 'values';
     let v;
     data.autoPopulated = data.autoPopulated || {};
-    await Promise.all(Object.entries(model[valuesKey] || {}).map(async ([k, def]) => {
-        v = await buildValueGenerator(<any>def, dir)(data);
-        if ('**unchanged**' !== v) {
-            if ('**clear**' === v) {
-                data.data[k] = undefined;
-            } else {
-                data.data[k] = v;
-            }
-            data.autoPopulated[k] = true;
+    const MAX_LOOP = 20;
+    const passes = Object.entries(model[valuesKey] || {}).reduce((acc, [k, v]: [string, any]) => {
+        v.pass = v.pass || 1;
+        const kk = Number(v.pass);
+        let ii = 0;
+        while (acc.length < kk) {
+            if (ii > MAX_LOOP) throw new Error(`Too many populate passes for ${k}`);
+            acc.push([]);
+            ii++;
         }
-    }));
+        acc[kk - 1].push([k, v]);
+        return acc;
+    }, [] as any[])
+
+    await passes.reduce(async (acc, items) => {
+        await acc;
+        return Promise.all(items.map(async ([k, def]) => {
+            v = await buildValueGenerator(<any>def, dir)(data);
+            if ('**unchanged**' !== v) {
+                if ('**clear**' === v) {
+                    data.data[k] = undefined;
+                } else {
+                    data.data[k] = v;
+                }
+                data.autoPopulated[k] = true;
+            }
+        }));
+    }, Promise.resolve(undefined));
     return data;
 }
