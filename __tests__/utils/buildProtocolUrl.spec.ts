dd893abe3f7d22167cdf66b585a78b51d8d3e19b
Olivier Hoareau <oha@greenberets.io>
2025-11-10 11:28:13 +0100

    fix: fix

--- __tests__/utils/buildProtocolUrl.spec.ts
+++ __tests__/utils/buildProtocolUrl.spec.ts
@@ -1,5 +1,10 @@
 import buildProtocolUrl from "../../src/utils/buildProtocolUrl";
 
+const originalEnv = process.env;
+
+afterEach(() => {
+  process.env = originalEnv;
+});
 describe('buildProtocolUrl', () => {
   ([
       [undefined, 'xx', undefined, undefined, ''],
@@ -13,10 +18,13 @@ describe('buildProtocolUrl', () => {
       ['https://{{webservice}}.dev.example.com', 'webservice', 'myws,myprodws:/a/b', undefined, 'https://myws.dev.example.com/a/b'],
       ['https://{{webservice}}.dev.example.com', 'webservice', 'myws,myprodws:/a/b', 'dev', 'https://myws.dev.example.com/a/b'],
       ['https://{{webservice}}.dev.example.com', 'webservice', 'myws,myprodws:/a/b', 'prod', 'https://myprodws.dev.example.com/a/b'],
-    ] as [string|undefined, string, string|undefined, string|undefined, string|undefined][])
+      ['https://{{webservice}}.dev.example.com', 'webservice', 'myws,^XYZ_ENDPOINT:/a/b', 'prod', 'https://myxyzendpoint.com/a/b', {XYZ_ENDPOINT: 'https://myxyzendpoint.com'}],
+      ['https://{{webservice}}.dev.example.com', 'webservice', 'myws,^https__//myop.com/xc/d:/a/b', 'prod', 'https://myop.com/xc/d/a/b'],
+    ] as [string|undefined, string, string|undefined, string|undefined, string|undefined, Record<string, string>?][])
         .forEach(
-            ([pattern, varName, value, env, expected]: [string|undefined, string, string|undefined, string|undefined, string|undefined]) => {
+            ([pattern, varName, value, env, expected, envs]: [string|undefined, string, string|undefined, string|undefined, string|undefined, Record<string, string>?]) => {
               it(`${pattern} + ${varName} + ${value} + ${env} => ${expected}`, () => {
+                !!envs && (process.env = {...originalEnv, ...envs});
                 expect(buildProtocolUrl(pattern, varName, value, env)).toEqual(expected);
               })
             }
--- src/utils/buildProtocolUrl.ts
+++ src/utils/buildProtocolUrl.ts
@@ -2,7 +2,9 @@ export function buildProtocolUrl(pattern: string|undefined, varName: string, val
   let [s, uri = '']: [string, string?] = (value || '').split(':') as [string, string?];
   const [name, nameProd = ''] = s.split(',');
   const isProd = env === 'prod';
-  return `${(pattern || '').replace(new RegExp(`\{\{${varName}\}\}`, 'g'), (isProd ? nameProd : name) || name)}${uri}`;
+  const selectedName = (isProd ? nameProd : name) || name;
+  const realPattern = ('^' === selectedName?.[0]) ? ('https__//' === selectedName.slice(1, 10) ? (selectedName.slice(1).replace(/__/g, ':')) : process.env[selectedName.slice(1)]) : pattern;
+  return `${(realPattern || '').replace(new RegExp(`\{\{${varName}\}\}`, 'g'), selectedName)}${uri}`;
 }
 
 export default buildProtocolUrl;
